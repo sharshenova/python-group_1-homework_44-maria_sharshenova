24188ebd5f4230c119a45c11b591a16dfe6ab8ae
Maria Sharshenova <maria@sharshenov.com>
2018-12-21 11:31:21 +0600

    Add new fields to model Order

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from webapp.models import Employee, Food, Order, OrderFoods
 
-# Register your models here.
+
+class EmployeeInline(admin.StackedInline):
+    model = Employee
+
+
+class EmployeeAdmin(UserAdmin):
+    inlines = [EmployeeInline]
+
+
+class OrderFoodsInline(admin.TabularInline):
+    model = OrderFoods
+    fields = ['food', 'amount']
+
+
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+    inlines = [OrderFoodsInline]
+
+
+admin.site.unregister(User)
+admin.site.register(User, EmployeeAdmin)
+admin.site.register(Food)
+admin.site.register(Order, OrderAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT, related_name='employee', verbose_name='Пользователь')
+    phone = models.CharField(max_length=50, verbose_name='Телефон')
+
+
+class Food(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Название')
+    description = models.TextField(max_length=1000, null=True, blank=True, verbose_name='Описание')
+    photo = models.ImageField(verbose_name='Фотография')
+    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Цена')
+
+
+class Order(models.Model):
+    STATUS_NEW = 'new'
+    STATUS_PREPARING = 'preparing'
+    STATUS_ON_WAY = 'on_way'
+    STATUS_DELIVERED = 'delivered'
+    STATUS_CANCELED = 'canceled'
+
+    STATUS_CHOICES = (
+        (STATUS_NEW, 'Новый'),
+        (STATUS_PREPARING, 'Готовится'),
+        (STATUS_ON_WAY, 'В пути'),
+        (STATUS_DELIVERED, 'Доставлен'),
+        (STATUS_CANCELED, 'Отменён')
+    )
+
+    contact_phone = models.CharField(max_length=50, verbose_name='Контактный телефон')
+    contact_name = models.CharField(max_length=100, verbose_name='Имя клиента')
+    delivery_address = models.CharField(max_length=200, null=True, blank=True, verbose_name='Адрес доставки')
+    status = models.CharField(max_length=20, default=STATUS_NEW, verbose_name='Статус', choices=STATUS_CHOICES)
+    operator = models.ForeignKey(User, null=True, blank=True, related_name='orders', verbose_name='Оператор',
+                                 on_delete=models.PROTECT)
+    courier = models.ForeignKey(User, null=True, blank=True, related_name='delivered', verbose_name='Курьер',
+                                on_delete=models.PROTECT)
+
+
+
+class OrderFoods(models.Model):
+    order = models.ForeignKey(Order, related_name='foods', verbose_name='Заказ', on_delete=models.PROTECT)
+    food = models.ForeignKey(Food, related_name='+', verbose_name='Блюдо', on_delete=models.PROTECT)
+    amount = models.IntegerField(verbose_name='Количество')
